8a8d9f4f16ee75d23e5bfa869236dc0c13887521
shynkaaaa <112634996+shynkaaaa@users.noreply.github.com>
2023-02-11 16:47:35 +0600

    Create Animal.java

---
+++ problem-A/Animal.java
@@ -0,0 +1,10 @@
+public class Animal {
+    protected String name;
+    Animal(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Animal[name=" + name + "]";
+    }
+}
